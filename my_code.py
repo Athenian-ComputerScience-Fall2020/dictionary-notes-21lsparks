f2c597aa4f33e8251e9a9dfd7545f32c5e0547b0
lspar <21lsparks@athenian.org>
2020-12-05 19:38:18 -0800

    took notes

--- my_code.py
+++ my_code.py
@@ -1,2 +1,25 @@
 # Use this to take notes on the Edpuzzle video. Try each example rather than just watching it - you will get much more out of it!
 #  
+student = {'name':'John', 'age':'25', 'courses':{'math', 'compsci'}}
+
+
+student['phone'] = '555=5555'
+student['name'] = 'Jane'
+student.update({'name': 'Jane, 'age': 26, 'phone': '555-5555'})
+del student['age']
+age = student.pop('age')
+print(student['courses'])
+
+print(student[1])
+
+print(student.get('name')
+print(student.get('phone', 'Not Found'))
+print(student)
+
+
+print(len(student)
+print(student.keys())
+print(student.values())
+print(student.items())
+for key,value in student:
+    print(key)
